7b8336dde6485bdedce02118f5ab016293d40a16
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 21:56:22 +0200

    rs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,37 +11,11 @@
 
   import React from "react";
 import ReactDOM from "react-dom";
-import App from "./comonents/App.jsx";
-function Card(props) {
-  return (
-    <div>
-      <div className="card">
-        <div className="top">
-          <h2 className="name"> {props.name}</h2>
-          <img
-            className="circle-img"
-            id="img"
-            src={props.src}
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">{props.number}</p>
-          <p className="info">{props.email}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+
+
 ReactDOM.render(
   <div>
-    <App />
-    <Card
-      name="Monkey D Luffy"
-      src="https://i.pinimg.com/originals/53/1b/f2/531bf28ee274611ab3b887c9c301d88a.jpg"
-      email="Yonko"
-      number="Gear 5"
-    />
+    <h1>Hello world !</h1>
   </div>,
   document.getElementById("root")
 );
