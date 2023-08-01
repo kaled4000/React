07fec76a8b7c06a16dee5c61996de1a630c3bce2
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 16:15:41 +0200

    rs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,8 @@
 import  React  from "react";
 import  ReactDOM  from "react-dom";
 
-const style ={ 
-  color: "red",
-  fontSize:"50px",
-  border:"10px solid black"
-}
 
 ReactDOM.render(<div>
-<h1 style= {style}>Hello world !</h1>
+<h1>Hello world </h1>
 </div>
 ,document.getElementById("root"));
