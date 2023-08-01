8748126e0627b0897795947418fb8dd9891ee58a
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 14:52:37 +0200

    JSX & ES6 Linerals

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,14 @@
 import  React  from "react";
 import  ReactDOM  from "react-dom";
 
-ReactDOM.render(<div>
-<h1>welcom to master react app</h1>
+const fname ="Zoro vs ";
+const lname ="King";
+const age = 39;
 
+
+ReactDOM.render(<div>
+<h1>welcom to {fname+lname}</h1>
+<h2>my age is {age}</h2>
+<p>you can write any js expression isnside carlecrass {Math.floor(Math.random()*10)}</p>
 </div>
 ,document.getElementById("root"));
