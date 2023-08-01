addf8c8f1f351c12871132b2b13c1fa6d6f5043d
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 21:30:59 +0200

    Keeper App project v1

---
+++ my-app/src/component/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>this is a title</h1>
+      <p>this is a content</p>
+    </div>
+  );
+}
+
+export default Note;
