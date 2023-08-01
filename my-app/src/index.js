d30573eabccef6ea15a01d9cb43d51d39e4db8f4
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 21:22:55 +0200

    Import  & Exports

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,14 @@ import  React  from "react";
 import  ReactDOM  from "react-dom";
 
 
+import pi,{doublepi,thridenpi} from "./math";
 ReactDOM.render(
 <div>
-   <h1>Hello world</h1>
+   <h1>Import & Export </h1>
+   <ul>
+      <li>{pi}</li>
+      <li>{doublepi}</li>
+      <li>{thridenpi}</li>
+   </ul>
 </div>
 ,document.getElementById("root"));
---
+++ my-app/src/math.js
@@ -0,0 +1,11 @@
+
+const pi = 3.1429;
+
+const doublepi = pi*2;
+
+const thridenpi = pi*3;
+
+export default pi;
+
+export {doublepi,thridenpi};
+
