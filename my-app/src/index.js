a45bacf39df6ff24481168586aca251aa6eb8f05
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 14:41:50 +0200

    rs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,6 @@ import  ReactDOM  from "react-dom";
 
 ReactDOM.render(<div>
 <h1>welcom to master react app</h1>
-<ul>
-    <li>sfasf</li>
-    <li>asdf</li>
-    <li>asfdas</li>
-</ul>
+
 </div>
 ,document.getElementById("root"));
