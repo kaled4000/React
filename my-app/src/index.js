c7d658afa8a782e30ed5adf42e88073f2e6b92b2
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 16:18:47 +0200

    React style Practice

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,35 @@
-import  React  from "react";
-import  ReactDOM  from "react-dom";
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
 
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>
-<h1>Hello world </h1>
-</div>
-,document.getElementById("root"));
+const d = new Date();
+const c = d.getHours();
+let dyStyle = { color: "" };
+let text;
+if (c < 12) {
+  text = "Good Morining";
+  dyStyle.color = "red";
+} else if (18 > c && c >= 12) {
+  text = "Good Afternoon";
+  dyStyle.color = "green";
+} else {
+  text = "Good Night";
+  dyStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={dyStyle}>
+      {" "}
+      {text}{" "}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
