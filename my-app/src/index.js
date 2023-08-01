520dfa8e6daf04d76507123be66558c82db6204a
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 14:53:40 +0200

    rs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,9 @@
 import  React  from "react";
 import  ReactDOM  from "react-dom";
 
-const fname ="Zoro vs ";
-const lname ="King";
-const age = 39;
 
 
 ReactDOM.render(<div>
-<h1>welcom to {fname+lname}</h1>
-<h2>my age is {age}</h2>
-<p>you can write any js expression isnside carlecrass {Math.floor(Math.random()*10)}</p>
+<h1>welcom to master React app</h1>
 </div>
 ,document.getElementById("root"));
