206ce0a5fabea3e0f04ee86a3a065cec11a773e9
king_of_hell <mhmdk15600@gmail.com>
2023-08-01 15:03:01 +0200

    JSX Practice

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
 import  React  from "react";
 import  ReactDOM  from "react-dom";
 
-
+const name = "king of hell ";
+const cureent_year= new Date().getFullYear();;
 
 ReactDOM.render(<div>
-<h1>welcom to master React app</h1>
+<h2> created by {name}</h2>
+<h3>copyright &copy; {cureent_year}</h3>
 </div>
 ,document.getElementById("root"));
