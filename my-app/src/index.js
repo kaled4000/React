0dae68e9f24b16633ff0bd873a9628041b67ae91
king_of_hell <mhmdk15600@gmail.com>
2023-08-02 22:10:56 +0200

    rs  React conditional Rendering

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./componet/App.jsx";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
