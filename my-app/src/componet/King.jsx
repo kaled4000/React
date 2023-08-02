3a8bbe2847ec4fbd72f5fe1762c3fd508e9c3b2e
king_of_hell <mhmdk15600@gmail.com>
2023-08-02 11:23:34 +0200

    Mapping components Practice

--- my-app/src/componet/King.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function King(props) {
-  return <img className="circle-img" src={props.img} alt="avatar_img" />;
-}
-
-export default King;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./componet/App.jsx";
 
-ReactDOM.render(<div>
-  <h1>Hello world!</h1>
-</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
