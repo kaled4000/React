3009300e81c2bd1b023f60e1641114af99449732
king_of_hell <mhmdk15600@gmail.com>
2023-08-02 16:18:41 +0200

    Mapping filter Readuce challenge (complete)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./componet/App.jsx";
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import App from "./componet/App.jsx";
+import emojipedia from "./emojipedia.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// 1 log specfic array
+
+//console.log(emojipedia);
+
+// log secfic elemnt inside object inside array
+// log only text
+const mean=emojipedia.map(function (element)  {
+    return element.meaning.substring(0,100)
+});
+console.log(mean)
+
+
