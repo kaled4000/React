70dbe13c780390ec558c17768d09755dabb137e3
king_of_hell <mhmdk15600@gmail.com>
2023-08-02 11:08:49 +0200

    rs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./componet/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<div>
+  <h1>Hello world!</h1>
+</div>, document.getElementById("root"));
