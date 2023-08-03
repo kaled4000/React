7bcdcea218bfeea03575ba02a811cd7e9a2b12bb
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 17:24:56 +0200

    Event Hooks start

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,46 +1,5 @@
-
-// console.log(animal);
-// const [cat,dog] =animal;
-// console.log(cat);
-// const {name:catName,sound:catSound ,Yonko:oldYonko}=cat;
-// console.log(oldYonko);
-// CHALLENGE: uncomment the code below and see the car stats rendered
-
 import React from "react";
 import ReactDOM from "react-dom";
-import cars from "./practice.js";
+import App from "./componet/App.jsx";
 
-const [Honda,Tesl] = cars;
-
-const {
-    speedStats:{topSpeed : hondaTopSpeed}
-} = Honda;
-
-const {speedStats : {topSpeed : teslTopSpeed } }= Tesl
-
-const {
-    coloursByPopularity:[hondaTopColour]
-} = Honda;
-const {
-    coloursByPopularity:[teslTopColour]
-} = Tesl;
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-       <th>Top Colour</th>
-    </tr>
-    <tr>
-      <td>{Tesl.model}</td>
-      <td>{teslTopSpeed}</td>
-      <td>{teslTopColour}</td>
-    </tr>
-    <tr>
-      <td>{Honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
