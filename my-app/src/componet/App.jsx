9dbe4b454363433b2a9f1d9290e68c4916983192
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 17:58:23 +0200

    Event Handling Complete

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [headingText, setHeadingText] =useState("King of Hell")
+    const [isMouseOver , setMouseOver] = useState(false);
+    function HandelClick(){
+        //console.log("clicked")
+    setHeadingText("King")    
+    }
+
+    function HandleMouseOver(){
+     //console.log("White");
+    setMouseOver(true) 
+    }
+
+     function HandleMouseOut(){
+        setMouseOver(false);
+     }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor: isMouseOver ? "black" : "white"}} 
+      onClick={HandelClick}
+      onMouseOver={HandleMouseOver}
+      onMouseOut={HandleMouseOut}>Submit</button>
     </div>
   );
 }
