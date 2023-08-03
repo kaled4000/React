146e0a5883279d7dd6f56d19fd11778f9712d0e0
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 13:39:46 +0200

    ES6 Object and Arry parctice

---
+++ my-app/src/data.js
@@ -0,0 +1,13 @@
+const animals = [
+    { 
+    name: "cat", 
+    sound: "meow",
+    model: [herbivores, omnivores, carnivores] },
+    { 
+    name: "dog", 
+    sound: "woof" 
+}
+  ];
+  
+  export default animals;
+  
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./componet/App";
+import animal from "./data.js";
+
+const animal_name = animal.map((name) => name.name);
+const animal_sound = animal.map((name) => name.sound);
+
+console.log(animal_name);
+console.log(animal_sound);
 
 
-ReactDOM.render(<App />,document.getElementById("root"));
 
+
+
+
+
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//        <th>Top Colour</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{tesla.TopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{honda.TopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
