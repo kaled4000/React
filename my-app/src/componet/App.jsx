c2cfc9cdc4f0fa446a69f69173fcc5b73c983c5c
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 11:57:40 +0200

    use Hooks practice (complete)

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,22 +1,25 @@
-import React ,{useState}from "react";
-//import ReactDOM  from "react";
-function App(){
+import React, { useState } from "react";
+//import react-dom from "react-dom";
 
-  const [count, setCount] = useState[0];
-  function Up(){
-    return setCount(count + 1);
-  }
-  function Down (){
-    return setCount (count - 1);
-  }
-    return( <div className="container">
-  <h1>{count}</h1>
-  <button onClick={Up}>-</button>
-  <button onClick={Down}>+</button>
+function App() {
+  // function sayHi() {
+  //   console.log("Hey");
+  // }
+  setInterval(Times, 1000);
+  let now = new Date().toLocaleTimeString();
 
-</div>
-    );
- 
+  const [time, Update] = useState(now);
+  //console.log(now);
+  function Times() {
+    const current_time = new Date().toLocaleTimeString();
+    Update(current_time);
+  }
+  return (
+    <div className="container">
+      <h1>{now}</h1>
+      <button onClick={Times}>Get Time</button>
+    </div>
+  );
 }
 
 export default App;
