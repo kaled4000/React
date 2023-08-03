4057cd70e1e912305bf9bca07ae75ffd3aa68745
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 18:00:48 +0200

    React forms start

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-    const [headingText, setHeadingText] =useState("King of Hell")
-    const [isMouseOver , setMouseOver] = useState(false);
-    function HandelClick(){
-        //console.log("clicked")
-    setHeadingText("King")    
-    }
-
-    function HandleMouseOver(){
-     //console.log("White");
-    setMouseOver(true) 
-    }
-
-     function HandleMouseOut(){
-        setMouseOver(false);
-     }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
+      <h1>Hello </h1>
       <input type="text" placeholder="What's your name?" />
-      <button style={{backgroundColor: isMouseOver ? "black" : "white"}} 
-      onClick={HandelClick}
-      onMouseOver={HandleMouseOver}
-      onMouseOut={HandleMouseOut}>Submit</button>
+      <button>Submit</button>
     </div>
   );
 }
