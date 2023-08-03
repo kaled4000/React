ede3ae5773ceb33c73c06de4497a8121e8c9a138
king_of_hell <mhmdk15600@gmail.com>
2023-08-03 18:33:26 +0200

    React forms compelet

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [name,setName]=useState("");
+    const [fullname,setfullname]= useState("");
+
+    function HandleChange(event){
+        console.log(event.target.value);
+        setName(event.target.value)
+    }
+    function HandleClick(){
+      setfullname(name);
+    }
+    //console.log(HandleClick)
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {fullname} </h1>
+      <input value={name} onChange={HandleChange} type="text" placeholder="What's your name?" />
+      <button onClick={HandleClick}>Submit</button>
     </div>
   );
 }
