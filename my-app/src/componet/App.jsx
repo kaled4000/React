e6520a7179993f7adab5dcd4b5380aca496b4735
king_of_hell <mhmdk15600@gmail.com>
2023-08-04 15:00:47 +0200

    Spread Opreation practice (Start)

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,52 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-  setContact(prev =>{ 
-    return {
-      ...prev,
-      [name]:value
-    };
-     });
-  }
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          value={contact.fName}
-          placeholder="First Name"
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          value={contact.lName}
-          placeholder="Last Name"
-        />
-        <input
-          onChange={handleChange}
-          name="email"
-          value={contact.email}
-          placeholder="Email"
-        />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
