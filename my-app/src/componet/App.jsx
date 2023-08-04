7c9a71c2309640ffc217391758b61fabdd1496b1
king_of_hell <mhmdk15600@gmail.com>
2023-08-04 16:22:37 +0200

    Spread Opreation Practice (complete)

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+
+  const [inputText,SetinputText] =useState("");
+  const [items,SetItems] = useState([])
+  function HandleChange(event){
+   const newvalue =event.target.value;
+   SetinputText(newvalue);
+  }
+  function AddItem(){
+    SetItems((prevItems) => {
+     return [...prevItems,inputText]
+    });
+    SetinputText("")
+  }
+ 
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={inputText} 
+        onChange={HandleChange} />
+        <button
+        onClick={AddItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((to_do_item) => <li>{to_do_item}</li>)}
         </ul>
       </div>
     </div>
