d14bfef95a7e9a43cbd4210d262c9572a16378e2
king_of_hell <mhmdk15600@gmail.com>
2023-08-04 10:46:44 +0200

    Challang complex states (start)

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-    const [name,setName]=useState("");
-    const [fullname,setfullname]= useState("");
-
-    function HandleChange(event){
-        console.log(event.target.value);
-        setName(event.target.value)
-    }
-    function HandleClick(){
-      setfullname(name);
-    }
-    //console.log(HandleClick)
   return (
     <div className="container">
-      <h1>Hello {fullname} </h1>
-      <input value={name} onChange={HandleChange} type="text" placeholder="What's your name?" />
-      <button onClick={HandleClick}>Submit</button>
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
