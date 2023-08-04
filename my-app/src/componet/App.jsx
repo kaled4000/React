145d1207d0a10c7940f5200ed2c47d34c6430128
king_of_hell <mhmdk15600@gmail.com>
2023-08-04 14:33:07 +0200

    soultion 1

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -7,46 +7,57 @@ function App() {
     email: ""
   });
 
-  function HandleChange(event){
-       const {name,value} = event.target
-       //console.log(newvalue + inputName)
-      
-       setContact((prev)=> {
-         if (name === "fName") {
-          return {
-            fName:value,
-            lName:prev,
-            email:prev.email
-          };
-        }if(name === "lName") {
-          return{
-            fName:prev.fName,
-            lName:value,
-            email:prev.email
-          };}
-         else if (name === "email") {
-          return {
-            fName:prev.fName,
-            lName:prev,
-            email:value.email};}
-                 ;})
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
   }
+
   return (
     <div className="container">
-      <h1>Hello 
-        {contact.fName}{contact.lName}
+      <h1>
+        Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
       <form>
-      <input name="fName" 
-      value={contact.fName}
-      onChange={HandleChange}placeholder="First Name" />
-      <input name="lName" 
-      value={contact.lName}
-      onChange={HandleChange}placeholder="Last Name" />
-      <input name="email" 
-      value={contact.email}
-      onChange={HandleChange}placeholder="Email" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
         <button>Submit</button>
       </form>
     </div>
@@ -54,6 +65,92 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [contact, setContact] = useState({
+//     fName: "",
+//     lName: "",
+//     email: ""
+//   });
+
+//   function HandleChange(event){
+//        const {name,value} = event.target
+//        //console.log(newvalue + inputName)
+      
+//        setContact((prev)=> {
+//          if (name === "fName") {
+//           return {
+//             fName:value,
+//             lName:prev,
+//             email:prev.email
+//           };
+//         }if(name === "lName") {
+//           return{
+//             fName:prev.fName,
+//             lName:value,
+//             email:prev.email
+//           };}
+//          else if (name === "email") {
+//           return {
+//             fName:prev.fName,
+//             lName:prev,
+//             email:value.email};}
+//                  ;})
+//   }
+//   return (
+//     <div className="container">
+//       <h1>Hello 
+//         {contact.fName}{contact.lName}
+//       </h1>
+//       <p>{contact.email}</p>
+//       <form>
+//       <input name="fName" 
+//       value={contact.fName}
+//       onChange={HandleChange}placeholder="First Name" />
+//       <input name="lName" 
+//       value={contact.lName}
+//       onChange={HandleChange}placeholder="Last Name" />
+//       <input name="email" 
+//       value={contact.email}
+//       onChange={HandleChange}placeholder="Email" />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
