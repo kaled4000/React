583f69aa6e66d4020c3f42c174abafea406ffc0d
king_of_hell <mhmdk15600@gmail.com>
2023-08-04 11:13:01 +0200

    Soulltion_1

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+
+  const [fname,SetNname] =useState("");
+  const [lname,SetLname] =useState("");
+  const [lname_2,SetLname_2]=useState(lname);
+
+  function handlechange(e){
+    SetNname(e.target.value)
+  }
+  function handl(e){
+    SetLname(e.target.value)
+  }
+  function HandleSubmit(event){
+   SetLname_2(lname)
+   event.preventDefault();
+  }
+
+ 
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fname} {lname_2}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input name="fName" onChange={handlechange} placeholder="First Name" />
+        <input name="lName" onChange={handl} placeholder="Last Name" />
+        <button onClick={HandleSubmit}  >Submit</button>
       </form>
     </div>
   );
