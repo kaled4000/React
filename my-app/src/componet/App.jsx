61c901749b62cf44760e610456183bb32c03b639
king_of_hell <mhmdk15600@gmail.com>
2023-08-06 17:05:57 +0200

    Keeper App part_3

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./Create";
 
 function App() {
   return (
     <div>
-      Test
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
   );
 }
