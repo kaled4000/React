c25329058e444eb0f0a12beffd8b8adef972bb87
king_of_hell <mhmdk15600@gmail.com>
2023-08-08 13:49:34 +0200

    Keeper App part 3 (Completed)

--- my-app/src/componet/App.jsx
+++ my-app/src/componet/App.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./Create";
 
+//1 track change on input
+//2 save item in arry 
+// 3 show all item from arraya 
+// 4 delete item when user click on delete from the
+// 5 render another item in the array
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- my-app/src/componet/Create.jsx
+++ my-app/src/componet/Create.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
-  return (
-    <div>
+  function CreateArea(Props){
+
+    const [note,SetNote] = useState({
+      title:"",
+      content:""
+    })
+
+    function handleChange(event){
+     
+      const {name,value} = event.target
+     
+      SetNote(prevnote => {
+        return{
+          ...prevnote,
+          [name]:value
+        }
+      })
+    }
+    function SubmitNote(event){
+      Props.onAdd(note)
+      SetNote({
+        title:"",
+        content:""
+      })
+  
+   event.preventDefault();
+    }
+    
+    return (
+      <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input 
+        name="title" 
+        value={note.title} 
+        placeholder="Title"
+       onChange={handleChange} />
+        <textarea 
+        name="content"
+        value={note.content} 
+        placeholder="Take a note..."
+         onChange={handleChange}
+        rows="3" />
+        <button onClick={SubmitNote}>Add</button>
       </form>
     </div>
   );
 }
+
 
 export default CreateArea;
